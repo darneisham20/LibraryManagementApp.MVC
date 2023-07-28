caf788fa203bb4a0f223074a7fbd661dc661812f
Darneisha Miller <darneisham20@gmail.com>
2023-07-28 16:19:48 -0400

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LibraryManagementApp.MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
